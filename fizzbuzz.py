2afc1ef1888f61b3fd4a1bbf65ba4a0fe20fde05
the-Fish2 <nishkamk2@gmail.com>
2024-10-20 11:24:38 -0700

    fizzbuzz unit tests

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+def fizzbuzz(i):
+    if i % 15 == 0:
+        return "FizzBuzz"
+    elif i % 5 == 0:
+        return "Buzz"
+    elif i % 3 == 0:
+        return "Fizz"
+    else:
+        return i
+    
+def main():
+    for i in range(1, 101):
+        print(fizzbuzz(i))
+
+if __name__ == '__main__':
+    main()
---
+++ tests.py
@@ -0,0 +1,19 @@
+import unittest
+from fizzbuzz import fizzbuzz
+
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_fizz(self):
+        for i in [3, 6, 9]:
+            assert fizzbuzz(i) == "Fizz"
+    
+    def test_buzz(self):
+        for i in [10, 20, 35]:
+            assert fizzbuzz(i) == "Buzz"
+
+    def test_fizzbuzz(self):
+        for i in [15, 90, 150]:
+            assert fizzbuzz(i) == "FizzBuzz"
+
+if __name__ == '__main__':
+    unittest.main()
